aa7ebfd4a7c527f8e5350c2184fbe576e6da2cd3
tatparya <tatparya.shankar@gmail.com>
2014-09-01 11:48:35 -0400

    Finished coding

--- PA01/answer01.c
+++ PA01/answer01.c
@@ -3,25 +3,92 @@
 
 int arraySum(int * array, int len)
 {
-    return 0;
+	int i;
+	int sum = 0;
+	for ( i = 0; i < len; i++ )
+	{
+		sum += array[i];
+	} 
+	return sum;
 }
 
 int arrayCountNegative(int * array, int len)
 {
-    return 0;
+	int i;
+	int ct = 0;
+	for ( i = 0; i < len; i++ )
+	{
+		if ( array[i] < 0 )
+		{
+			ct++;
+		}
+	}
+	return ct;
 }
 
 int arrayIsIncreasing(int * array, int len)
 {    
-    return 0;
+    
+	int i;
+	int result;
+	int flag = 0;
+	for ( i = 0; i < len - 1; i++ )
+	{
+		if ( array[i] > array[i + 1] )
+		{
+			flag++;
+		}
+	}
+	
+	if ( !flag )
+	{
+		result = 1;
+	}
+	else
+	{
+		result = 0;
+	}
+	return result;
 }
 
 int arrayIndexRFind(int needle, const int * haystack, int len)
 {
-    return 0;
+    int i = len - 1;
+	int index;
+	int found = 0;
+	while ( ( i >= 0 ) && ( !found ) )
+	{
+		if ( haystack[i] == needle )
+		{
+			index = i;
+			found++;
+		}
+		i--;
+	}
+
+	if ( !found )
+	{
+		index = -1;
+	}
+
+	return index;
 }
 
 int arrayFindSmallest(int * array, int len)
 {
-    return 0;
+    int smallest = len - 1;
+	int i;
+	
+	for ( i = len - 2; i >= 0; i-- )
+	{
+		if ( array[i] <= array[smallest] )
+		{
+			smallest = i;
+		}
+	}
+	if ( len == 0 )
+	{
+		smallest = 0;
+	}
+	return smallest;
 }
