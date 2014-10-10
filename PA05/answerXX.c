a3da37a9d59ebd4c91fd14798a1327e565767e69
tatparya <tatparya.shankar@gmail.com>
2014-10-10 00:46:41 -0400

    Started working on PA05

---
+++ PA05/answerXX.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "answer05.h"
+
+//	1
+void partitionAll ( int value )
+{
+	
+}
+
+void partitionAll_fn ( int * arr, int index, int value ) 
+{
+
+}
+
+//	2
+void partitionIncreasing ( int value )
+{
+	
+}
+
+void partitionIncreasing_fn ( int * arr, int index, int value ) 
+{
+
+}
+
+//	3
+void partitionDecreasing ( int value )
+{
+	
+}
+
+void partitionDecreasing_fn ( int * arr, int index, int value ) 
+{
+
+}
+
+//	4
+void partitionOdd ( int value )
+{
+	
+}
+
+void partitionOdd_fn ( int * arr, int index, int value ) 
+{
+
+}
+
+//	5
+void partitionEven ( int value )
+{
+	
+}
+
+void partitionEven_fn ( int * arr, int index, int value ) 
+{
+
+}
+
+//	6
+void partitionOddAndEven ( int value )
+{
+	
+}
+
+void partitionOddAndEven_fn ( int * arr, int index, int value ) 
+{
+
+}
+
+//	7
+void partitionPrime ( int value )
+{
+	
+}
+
+void partitionPrime_fn ( int * arr, int index, int value ) 
+{
+
+}
---
+++ PA05/partition.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "answer05.h"
+
+int main ( 
+
